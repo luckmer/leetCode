d7d153758625b57e70d557b9d60df0587568890b
luckmer <woszczo04@gmail.com>
2021-02-16 22:09:32 +0100

    III

--- LengthofLastWord.js
+++ LengthofLastWord.js
@@ -8,3 +8,7 @@ const lengthOfLastWord = (s) => {
     s= s.trim();
     return s.indexOf(' ') >= 0 ? s.slice(s.lastIndexOf(' '), s.length - 1).length : s.length
 };
+
+
+const lengthOfLastWord = (s) => s.trim().split(" ").pop().length;
+
