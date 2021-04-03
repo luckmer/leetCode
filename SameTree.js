276690f08e1b2656e4ab858805e48750cb26bab8
luckmer <woszczo04@gmail.com>
2021-04-03 21:51:51 +0200

    new code

--- SameTree.js
+++
@@ -1,7 +0,0 @@
-// Input: p = [1,2,3], q = [1,2,3]
-// Output: true
-
-const isSameTree = (p, q) => !p || !q ? 
-    p == q : p.val == q.val
-    && isSameTree(p.left, q.left) 
-    && isSameTree(p.right, q.right);
