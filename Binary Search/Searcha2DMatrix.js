57f5e034e9d830bdad51c50c3c79caf973218a06
luckmer <woszczo04@gmail.com>
2021-04-05 21:01:09 +0200

    Search a 2D Matrix

---
+++ Binary Search/Searcha2DMatrix.js
@@ -0,0 +1,25 @@
+// Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+// Integers in each row are sorted from left to right.
+// The first integer of each row is greater than the last integer of the previous row.
+
+// Input: (matrix = [
+//   [1, 3, 5, 7],
+//   [10, 11, 16, 20],
+//   [23, 30, 34, 60],
+// ]),
+//   (target = 3);
+// Output: true;
+
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+// Output: true
+
+const searchMatrix = (matrix, target) => {
+  for (let value of matrix) {
+    for (let check of value) {
+      if (check === target) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
